8f6c363d56908a7bcd105183583be9f8d035a925
Younes Jaaidi <younes@marmicode.io>
2025-04-03 01:05:04 +0200

    refactor(core): tidy up

--- packages/core/src/lib/extraction-writer/path-utils.ts
+++ packages/core/src/lib/extraction-writer/path-utils.ts
@@ -3,9 +3,9 @@ import { dirname, join, relative } from 'node:path/posix';
 /**
  * Adjust the relative import path when moving a file from `srcFilePath` to `destFilePath`.
  *
- * @param srcFilePath
- * @param destFilePath
- * @param importPath
+ * @param srcFilePath initial file path
+ * @param destFilePath the new file path after moving
+ * @param importPath the import path to adjust
  */
 export function adjustImportPath({
   srcFilePath,
