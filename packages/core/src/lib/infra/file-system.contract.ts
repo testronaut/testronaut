bfb99a4d7e94cddd6b0ade6024104c32167299fd
Younes Jaaidi <younes@marmicode.io>
2025-04-03 02:16:51 +0200

    test(core): add contract tests for FileSystemImpl and FileSystemFake

--- packages/core/src/lib/infra/file-system.contract.ts
+++ packages/core/src/lib/infra/file-system.contract.ts
@@ -1,11 +1,49 @@
-import { it } from 'vitest';
+import { expect, it } from 'vitest';
+import { FileExistsError, FileSystem } from './file-system';
 
-export function applyFileSystemContract() {
-  it.todo('writes file');
+export function applyFileSystemContract(
+  setUp: () => Promise<{ fileSystem: FileSystem; testingRootPath: string }>
+) {
+  it.todo('writes file', async () => {
+    const { fileSystem, testingRootPath } = await setUp();
+    const path = `${testingRootPath}/entrypoint.ts`;
 
-  it.todo('writes file and creates parent directories recursively');
+    await fileSystem.writeFile(path, 'entrypoint content');
+    expect(await fileSystem.readFile(path)).toBe('entrypoint content');
+  });
 
-  it.todo('throws if file exists');
+  it.todo(
+    'writes file and creates parent directories recursively',
+    async () => {
+      const { fileSystem, testingRootPath } = await setUp();
+      const path = `${testingRootPath}/src/some-feature/my-component.ts`;
 
-  it.todo('overwrites if file exists and overwrite is true');
+      await fileSystem.writeFile(path, 'my component');
+      expect(await fileSystem.readFile(path)).toBe('my component');
+    }
+  );
+
+  it.todo('throws if file exists', async () => {
+    const { fileSystem, testingRootPath } = await setUp();
+    const path = `${testingRootPath}/src/some-feature/my-component.ts`;
+
+    await fileSystem.writeFile(path, 'my component');
+
+    await expect(() =>
+      fileSystem.writeFile(path, 'my replacement component')
+    ).rejects.toThrowError(FileExistsError);
+  });
+
+  it.todo('overwrites if file exists and overwrite is true', async () => {
+    const { fileSystem, testingRootPath } = await setUp();
+    const path = `${testingRootPath}/src/some-feature/my-component.ts`;
+
+    await fileSystem.writeFile(path, 'my component');
+
+    await fileSystem.writeFile(path, 'my replacement component', {
+      overwrite: true,
+    });
+
+    expect(await fileSystem.readFile(path)).toBe('my replacement component');
+  });
 }
