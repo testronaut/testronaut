d42226a87f659672c073827875df3cac8aceb50d
Younes Jaaidi <younes@marmicode.io>
2025-04-03 02:03:46 +0200

    test(core): update test ideas for FileSystemImpl and FileSystemFake

---
+++ packages/core/src/lib/infra/file-system.contract.ts
@@ -0,0 +1,11 @@
+import { it } from 'vitest';
+
+export function applyFileSystemContract() {
+  it.todo('writes file');
+
+  it.todo('writes file and creates parent directories recursively');
+
+  it.todo('throws if file exists');
+
+  it.todo('overwrites if file exists and overwrite is true');
+}
