f73531dd79580eb7d79ee7e62d75c406d07eb492
Younes Jaaidi <younes@marmicode.io>
2025-04-04 19:23:43 +0200

    refactor(core): swap tmp implementations with real ones

--- packages/core/src/lib/runner/extraction-pipeline.ts
+++ packages/core/src/lib/runner/extraction-pipeline.ts
@@ -1,18 +1,14 @@
-import { createHash } from 'node:crypto';
 import { readFile } from 'node:fs/promises';
 import { analyze } from '../analyzer/analyze';
 import { ExtractionConfig } from '../extraction-writer/extraction-config';
 import { ExtractionWriter } from '../extraction-writer/extraction-writer';
 import { createFileInfo, FileInfo } from './file-info';
-import { TmpExtractionWriter } from './tmp-extraction-writer';
 
 export class ExtractionPipeline {
   readonly #extractionWriter: ExtractionWriter;
-  readonly #tmpExtractionWriter: TmpExtractionWriter;
 
   constructor(config: ExtractionConfig) {
     this.#extractionWriter = new ExtractionWriter(config);
-    this.#tmpExtractionWriter = new TmpExtractionWriter(config);
   }
 
   init() {
@@ -22,22 +18,13 @@ export class ExtractionPipeline {
   async extract(path: string): Promise<FileInfo> {
     const content = await readFile(path, 'utf-8');
 
-    const extractedFunctions = analyze({
-      path,
-      content,
+    const analysis = analyze({ path, content });
+
+    await this.#extractionWriter.write(analysis);
+
+    return createFileInfo({
+      path: analysis.path,
+      hash: analysis.hash,
     });
-
-    const fileInfo = createFileInfo({ hash: this.#computeHash(content), path });
-
-    await this.#tmpExtractionWriter.write({
-      ...fileInfo,
-      extractedFunctions,
-    });
-
-    return fileInfo;
-  }
-
-  #computeHash(content: string) {
-    return createHash('sha256').update(content).digest('base64').slice(0, 8);
   }
 }
