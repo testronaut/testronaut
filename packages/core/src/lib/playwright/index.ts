40d3c3e626174c1d24df5e81fed69646a3160e91
Younes Jaaidi <younes@marmicode.io>
2025-04-05 00:23:57 +0200

    refactor(core): tidy up

--- packages/core/src/lib/playwright/index.ts
+++ packages/core/src/lib/playwright/index.ts
@@ -1,5 +1,4 @@
 export { defineConfig, devices, expect } from '@playwright/test';
 export * from './fixtures';
 export * from './with-ct';
-export { PlaywrightCtOptions } from './options';
-export { Options } from './options';
+export { Options, PlaywrightCtOptions } from './options';
