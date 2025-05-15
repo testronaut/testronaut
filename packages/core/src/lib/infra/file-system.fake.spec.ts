64f18332fc968a526fc566d7132c6fcf82fa4bd8
Younes Jaaidi <younes@marmicode.io>
2025-05-15 02:55:16 +0200

    test(core): fix test suite's name
    Signed-off-by: Younes Jaaidi <younes@marmicode.io>

--- packages/core/src/lib/infra/file-system.fake.spec.ts
+++ packages/core/src/lib/infra/file-system.fake.spec.ts
@@ -1,9 +1,8 @@
 import { describe } from 'vitest';
-import { FileSystemImpl } from './file-system.impl';
 import { applyFileSystemContract } from './file-system.contract';
 import { FileSystemFake } from './file-system.fake';
 
-describe(FileSystemImpl.name, () => {
+describe(FileSystemFake.name, () => {
   applyFileSystemContract(async () => ({
     fileSystem: new FileSystemFake(),
     testingRootPath: '/my-fake-workspace',
