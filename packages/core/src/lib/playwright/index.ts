6c0aa45161d5fa19aa4d64ce7c1c63b3d0ad1959
Younes Jaaidi <younes@marmicode.io>
2025-05-22 23:54:04 +0200

    refactor: tidy up

--- packages/core/src/lib/playwright/index.ts
+++ packages/core/src/lib/playwright/index.ts
@@ -1,4 +1,4 @@
 export { defineConfig, devices, expect } from '@playwright/test';
-export * from './fixtures';
+export { test, type Fixtures, type RunInBrowser } from './fixtures';
 export type { Options, TestronautOptions } from './options';
-export * from './with-testronaut';
+export { withTestronaut, type WithTestronautParams } from './with-testronaut';
