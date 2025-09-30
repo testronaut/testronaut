6c79da276ed82f601966246c4094288dea13a1e1
Younes Jaaidi <younes@marmicode.io>
2025-09-30 21:52:00 +0200

    test: refactor vite/vitest config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,24 +1,26 @@
-import { defineConfig } from 'vite';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/core',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/packages/core',
-      provider: 'v8',
+export default mergeConfig(
+  defineConfig({
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/packages/core',
+    plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  }),
+  defineVitestConfig({
+    test: {
+      watch: false,
+      globals: true,
+      environment: 'node',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      reporters: ['default'],
+
+      coverage: {
+        reportsDirectory: '../../coverage/packages/core',
+        provider: 'v8',
+      },
     },
-  },
-});
+  })
+);
