7e7127a2cff1eeb5c6cc679f2bdb45ce8a1660e7
Younes Jaaidi <younes@marmicode.io>
2025-09-30 21:51:32 +0200

    test(angular): enable typechecking

--- packages/angular/vite.config.ts
+++ packages/angular/vite.config.ts
@@ -1,25 +1,30 @@
-/// <reference types='vitest' />
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { defineConfig } from 'vite';
+import { defineConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/packages/angular',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  test: {
-    watch: false,
-    globals: true,
-    environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/packages/angular',
-      provider: 'v8',
+export default mergeConfig(
+  defineConfig({
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/packages/angular',
+    plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  }),
+  defineVitestConfig({
+    test: {
+      typecheck: {
+        enabled: true,
+        tsconfig: 'tsconfig.spec.json',
+      },
+      watch: false,
+      globals: true,
+      environment: 'node',
+      include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+      reporters: ['default'],
+
+      coverage: {
+        reportsDirectory: '../../coverage/packages/angular',
+        provider: 'v8',
+      },
     },
-  },
-});
+  })
+);
