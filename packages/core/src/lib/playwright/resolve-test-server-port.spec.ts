8f218207abd93f6313dba1cfed9df9a3bf41954d
Younes Jaaidi <younes@marmicode.io>
2025-10-03 14:06:15 +0200

    test(core): add startServer fixture

--- packages/core/src/lib/playwright/resolve-test-server-port.spec.ts
+++ packages/core/src/lib/playwright/resolve-test-server-port.spec.ts
@@ -1,12 +1,35 @@
 import { resolveTestServerPort } from './resolve-test-server-port';
-import { describe, it } from 'vitest';
+import { describe, test as base } from 'vitest';
+import { createServer, Server } from 'node:http';
+
+const test = base.extend<{ startServer: StartServerFn }>({
+  // eslint-disable-next-line no-empty-pattern
+  startServer: async ({}, use) => {
+    let servers: Server[] = [];
+    await use((seed: string) => {
+      /* Using `resolveTestServer` ourselves here in the tests
+       * to start an existing server for tests who need it. */
+      const { port } = resolveTestServerPort(seed);
+      const server = createServer((_, res) => {
+        res.statusCode = 200;
+        res.end();
+      });
+      servers = [...servers, server];
+      return { port };
+    });
+  },
+});
+
+type StartServerFn = (seed: string) => { port: number };
 
 describe(resolveTestServerPort.name, () => {
-  it.todo('derives the port from the provided seed');
+  test('derives the port from the provided seed', () => {
+    // expect(server.port).toBe(7357);
+  });
 
-  it.todo('picks the next available port');
+  test.todo('picks the next available port');
 
-  it.todo('tells if another test server might be running');
+  test.todo('tells if another test server might be running');
 
-  it.todo('tells if no other test server is running');
+  test.todo('tells if no other test server is running');
 });
