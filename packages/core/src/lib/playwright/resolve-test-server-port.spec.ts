5286e6ff0b15067395bfcd0d019dbd4281b60b87
Younes Jaaidi <younes@marmicode.io>
2025-10-03 14:28:46 +0200

    test(core): add tests for resolving test server port

--- packages/core/src/lib/playwright/resolve-test-server-port.spec.ts
+++ packages/core/src/lib/playwright/resolve-test-server-port.spec.ts
@@ -1,35 +1,57 @@
-import { resolveTestServerPort } from './resolve-test-server-port';
-import { describe, test as base } from 'vitest';
 import { createServer, Server } from 'node:http';
+import { test as base, describe } from 'vitest';
+import { resolveTestServerPort } from './resolve-test-server-port';
 
 const test = base.extend<{ startServer: StartServerFn }>({
   // eslint-disable-next-line no-empty-pattern
   startServer: async ({}, use) => {
     let servers: Server[] = [];
-    await use((seed: string) => {
-      /* Using `resolveTestServer` ourselves here in the tests
-       * to start an existing server for tests who need it. */
-      const { port } = resolveTestServerPort(seed);
+    await use((port) => {
       const server = createServer((_, res) => {
         res.statusCode = 200;
         res.end();
       });
       servers = [...servers, server];
-      return { port };
+      server.listen(port);
     });
   },
 });
-
-type StartServerFn = (seed: string) => { port: number };
+type StartServerFn = (port: number) => void;
 
 describe(resolveTestServerPort.name, () => {
-  test('derives the port from the provided seed', () => {
-    // expect(server.port).toBe(7357);
+  test.todo('derives the port from the provided seed', () => {
+    const { port } = resolveTestServerPort(
+      '/users/me/my-project/playwright-testronaut.config.ts'
+    );
+
+    /* 8801 is the port computed from the provided seed. */
+    expect(port).toBe(8801);
   });
 
-  test.todo('picks the next available port');
+  test.todo('picks the next available port', ({ startServer }) => {
+    const seed = '/users/me/my-project/playwright-testronaut.config.ts';
+    startServer(8801);
+    startServer(8802);
+    startServer(8803);
 
-  test.todo('tells if another test server might be running');
+    const { port } = resolveTestServerPort(seed);
+    expect(port).toBe(8804);
+  });
 
-  test.todo('tells if no other test server is running');
+  test.todo(
+    'tells if another test server might be running',
+    ({ startServer }) => {
+      const seed = '/users/me/my-project/playwright-testronaut.config.ts';
+      startServer(8801);
+
+      const { anotherTestServerMightBeRunning } = resolveTestServerPort(seed);
+      expect(anotherTestServerMightBeRunning).toBe(true);
+    }
+  );
+
+  test.todo('tells if no other test server is running', () => {
+    const seed = '/users/me/my-project/playwright-testronaut.config.ts';
+    const { anotherTestServerMightBeRunning } = resolveTestServerPort(seed);
+    expect(anotherTestServerMightBeRunning).toBe(false);
+  });
 });
