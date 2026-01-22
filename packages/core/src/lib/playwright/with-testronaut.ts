c5ec0a6bba82b2fd6b877cdc8c273c4b4c2032b4
Younes Jaaidi <younes@marmicode.io>
2026-01-22 11:03:28 +0100

    refactor(core): remove is-server-running check which was broken anyway
    is-server-running.js package was not published with the package 😅

--- packages/core/src/lib/playwright/with-testronaut.ts
+++ packages/core/src/lib/playwright/with-testronaut.ts
@@ -1,6 +1,5 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import { spawnSync } from 'node:child_process';
-import { dirname, join } from 'node:path';
+import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { ExtractionWriter } from '../extraction-writer/extraction-writer';
 import type { FileSystem } from '../infra/file-system';
@@ -42,7 +41,6 @@ export function _internal_withTestronaut({
   transforms,
   fileSystem,
 }: _internal_WithTestronautParams): PlaywrightTestronautConfig {
-  const isServerRunningCmd = join(dirname(__filename), 'is-server-running.js');
   const projectRoot = dirname(configPath);
   const port = 7357;
 
@@ -59,20 +57,15 @@ export function _internal_withTestronaut({
    *   and extracted files.
    * - The entrypoint file is reset at it might indirectly import files that do not exist
    *   anymore. This happens often when the user switches to another git branch for example.
-   * - We do not overwrite this file after the server starts as it can cause race conditions.
-   *   This happens when running the tests on different browsers. Playwright keeps the same
-   *   web server but starts a different worker for each browser.
    *
    * The current workaround is to check if the web server has already started or not.
    *
-   * This is temporarily as the right solution is to control the test server ourselves.
+   * This is temporary as the right solution is to control the test server ourselves.
    *
    * Note that `globalSetup` sounds like the right place, but it runs after the web servers starts
    * and it can be easily mistakenly overriden by the user.
    * Cf. https://github.com/microsoft/playwright/issues/19571#issuecomment-1358368164 */
-  if (spawnSync(isServerRunningCmd, [port.toString()]).status !== 0) {
-    extractionWriter.resetEntrypoint();
-  }
+  extractionWriter.resetEntrypoint();
 
   return {
     testDir: 'src',
